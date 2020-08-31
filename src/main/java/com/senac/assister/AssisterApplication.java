936f885cce27be6bb00676191b019c6787944d8d
eopit <pedro.barricelli@gmail.com>
2020-08-31 19:09:09 -0300

    rocket: Initial commit

---
+++ src/main/java/com/senac/assister/AssisterApplication.java
@@ -0,0 +1,13 @@
+package com.senac.assister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/assister/AssisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.assister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
