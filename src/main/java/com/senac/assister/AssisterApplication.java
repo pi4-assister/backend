97c1ee28d1e7b5973de06eba2c40c5d2994efda6
eopit <pedro.barricelli@gmail.com>
2020-09-05 15:30:26 -0300

    adding swagger

--- src/main/java/com/senac/assister/AssisterApplication.java
+++ src/main/java/com/senac/assister/AssisterApplication.java
@@ -3,9 +3,6 @@ package com.senac.assister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.Instant;
-import java.util.UUID;
-
 @SpringBootApplication
 public class AssisterApplication {
 
