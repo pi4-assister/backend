e08476dfaa1f7977bddc9a234382377c222ff262
eopit <pedro.barricelli@gmail.com>
2020-11-17 23:10:13 -0300

    add template cron job

---
+++ src/main/java/com/senac/assister/backend/domain/cron/AssisterJobs.java
@@ -0,0 +1,15 @@
+package com.senac.assister.backend.domain.cron;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+public class AssisterJobs {
+    @Scheduled(cron = "0 0 12")
+    public void invalidateServices() {
+        System.out.println("Rodando teste no horário: " + Instant.now());
+    }
+}
+
