ad53127e7330fc954a03f19f09f68ee91112a106
Rafan <rafanarbutis@outlook.com>
2020-11-19 14:25:30 -0300

    Add lombok in dto ServiceResponseAlterStatus

--- src/main/java/com/senac/assister/backend/rest/dto/service/ServiceResponseAlterStatus.java
+++ src/main/java/com/senac/assister/backend/rest/dto/service/ServiceResponseAlterStatus.java
@@ -2,10 +2,17 @@ package com.senac.assister.backend.rest.dto.service;
 
 import com.senac.assister.backend.domain.entity.Service;
 import com.senac.assister.backend.domain.enumeration.ServiceStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.modelmapper.ModelMapper;
 
 import java.util.UUID;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ServiceResponseAlterStatus {
 
     private static final ModelMapper mapper = new ModelMapper();
